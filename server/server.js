30c65d4c79d83dab84372cff42f4b3d4801d9b5a
NANNAGARAM CHAREESH <22h51a05b1@cmrcet.ac.in>
2026-05-26 16:08:11 +0530

    fix express 5 wildcard

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get('*all', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 } else {
